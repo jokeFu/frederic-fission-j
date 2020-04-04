6248077a02a0e7c1c36d2607ea4c2fe15136ef4b
mysql <542532773@qq.com>
2020-04-05 01:42:55 +0800

    Squashed commit of the following:
    commit 520be6bb27def8709a1f9b1b5b033c553e273d73
    Author: Administrator <13555768695@163.com>
    Date:   Wed Feb 26 12:31:34 2020 +0800
    
        更改之前不能修改的问题，增加站点跟模板的动态获取

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/CmsSiteService.java
@@ -0,0 +1,27 @@
+package com.xuecheng.manage_cms.service;
+
+import com.xuecheng.framework.domain.cms.CmsSite;
+import com.xuecheng.framework.model.response.CommonCode;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.model.response.QueryResult;
+import com.xuecheng.manage_cms.dao.CmsSiteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CmsSiteService {
+
+    @Autowired
+    private CmsSiteRepository cmsSiteRepository;
+
+    public QueryResponseResult findAll() {
+        List<CmsSite> all = cmsSiteRepository.findAll();
+        QueryResult queryResult = new QueryResult();
+        queryResult.setList(all);
+        queryResult.setTotal(all.size());
+        return new QueryResponseResult(CommonCode.SUCCESS, queryResult);
+    }
+
+}
